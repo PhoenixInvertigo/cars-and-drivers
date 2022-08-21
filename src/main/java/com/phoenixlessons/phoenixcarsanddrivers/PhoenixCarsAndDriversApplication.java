3d2e5eab269aecb40a703f7556db9ff7883fd923
PhoenixInvertigo <60861253+PhoenixInvertigo@users.noreply.github.com>
2022-08-21 18:45:12 -0500

    Initial commit

---
+++ src/main/java/com/phoenixlessons/phoenixcarsanddrivers/PhoenixCarsAndDriversApplication.java
@@ -0,0 +1,13 @@
+package com.phoenixlessons.phoenixcarsanddrivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixCarsAndDriversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixCarsAndDriversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phoenixlessons/phoenixcarsanddrivers/PhoenixCarsAndDriversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenixlessons.phoenixcarsanddrivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixCarsAndDriversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
